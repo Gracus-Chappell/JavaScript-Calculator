ffb175862ed5be97131d3a92c3e0d2cc673e5dec
Gracus-Chappell <64798078+Gracus-Chappell@users.noreply.github.com>
2022-03-06 21:39:57 +0000

    Updated CSS layout & fine tuned calculations
    Updated the look of the calculator on screen as well as prevented errors in the calculator (0 multiplied by 0 / 0 divided by 0).

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-let calculation = [0.00, "0"];
+let calculation = [0, "0"];
 let operator = "";
 
 
+//Functionality buttons include Clear, negative/positive and Decimal point
 function buttonFunctions(clicked_id) {
   switch (clicked_id) {
     case "clear":
-      calculation = [0, ""];
+      calculation = [0, "0"];
       operator = "";
       document.getElementById("display").innerHTML = calculation[0];
       break;
@@ -16,44 +17,45 @@ function buttonFunctions(clicked_id) {
     case "decimalPoint":
       calculation[1].includes(".") == true ? "" : calculation[1] = calculation[1] + ".";
       document.getElementById("display").innerHTML = calculation[1];
-      console.log(calculation);
   }
 }
 
 //Chosen number is added to the calculation.
 function buttonNumbers(clicked_id) {
   calculation[1] == "0" ? calculation[1] = clicked_id : calculation[1] = calculation[1] + clicked_id;
+  calculation[1].length > 9 ? calculation[1] = calculation[1].substring(0,9) : "";
+  console.log(calculation[1]);
   document.getElementById("display").innerHTML = calculation[1];
-  console.log(calculation);
 }
 
 // Chosen operator is added to the calculation.
 function buttonOperator(clicked_id) {
   switch (operator) {
     case "add":
-      calculation[0] = calculation[0] + parseFloat(calculation[1]);
+      calculation[0] == 0 && calculation[1] == "0" ? calculation[0] = 0 : calculation[0] = calculation[0] + parseFloat(calculation[1]);
       calculation[1] = "0";
-      document.getElementById("display").innerHTML = calculation[0];
+      document.getElementById("display").innerHTML = calculation[0].toString().substring(0,9);
       console.log(calculation);
       break;
     case "subtract":
       calculation[0] = calculation[0] - parseFloat(calculation[1]);
       calculation[1] = "0";
-      document.getElementById("display").innerHTML = calculation[0];
+      document.getElementById("display").innerHTML = calculation[0].toString().substring(0,9);
       break;
     case "multiply":
-      calculation[0] = calculation[0] * parseFloat(calculation[1]);
-      calculation[1] = "";
-      document.getElementById("display").innerHTML = calculation[0];
+      calculation[0] == 0 && calculation[1] == "0" ? calculation[0] = 0 : calculation[0] = calculation[0] * parseFloat(calculation[1]);
+      calculation[1] = "0";
+      document.getElementById("display").innerHTML = calculation[0].toString().substring(0,9);
+      console.log(calculation)
       break;
     case "divide":
-      calculation[0] = calculation[0] / parseFloat(calculation[1]);
-      calculation[1] = "";
-      document.getElementById("display").innerHTML = calculation[0];
+      calculation[0] == 0 && calculation[1] == "0" ? (calculation[0] = 0, document.getElementById("display").innerHTML = "WHAT?!")
+      : (calculation[0] = calculation[0] / parseInt(calculation[1]), document.getElementById("display").innerHTML = calculation[0].toString().substring(0,9));
+      calculation[1] = "0";
       break;
     case "":
-      calculation[1] !== "" ? calculation[0] = parseFloat(calculation[1]) : "";
-      calculation[1] = "";
+      calculation[0] = parseFloat(calculation[1]);
+      calculation[1] = "0";
   }
   operator = clicked_id;
 }
@@ -62,20 +64,20 @@ function buttonCalculate() {
   switch (operator) {
     case "add":
       calculation[0] = calculation[0] + parseFloat(calculation[1]);
-      document.getElementById("display").innerHTML = calculation[0];
-      console.log(calculation);
+      document.getElementById("display").innerHTML = calculation[0].toString().substring(0,9);
       break;
     case "subtract":
       calculation[0] = calculation[0] - parseInt(calculation[1]);
-      document.getElementById("display").innerHTML = calculation[0];
+      document.getElementById("display").innerHTML = calculation[0].toString().substring(0,9);
       break;
     case "multiply":
       calculation[0] = calculation[0] * parseInt(calculation[1]);
-      document.getElementById("display").innerHTML = calculation[0];
+      document.getElementById("display").innerHTML = calculation[0].toString().substring(0,9);
       break;
     case "divide":
-      calculation[0] = calculation[0] / parseInt(calculation[1]);
-      document.getElementById("display").innerHTML = calculation[0];
+      calculation[0] == 0 && calculation[1] == "0" ? (calculation[0] = 0, document.getElementById("display").innerHTML = "WHAT?!")
+      : (calculation[0] = calculation[0] / parseInt(calculation[1]), document.getElementById("display").innerHTML = calculation[0].toString().substring(0,9));
+      console.log(calculation);
       break;
     case "":
   }
