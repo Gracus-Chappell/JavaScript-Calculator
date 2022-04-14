dfffcf706e2a0a8bec77d7ee3fa139b8b922581c
Gracus-Chappell <64798078+Gracus-Chappell@users.noreply.github.com>
2022-04-14 01:19:51 +0100

    Changed array to variables
    Changed the calculation array to two seperate variables for easy-to-read code.
    
    Fixed the calculate button to allow the user to use the calculated answer as the first number.

--- js/main.js
+++ js/main.js
@@ -1,88 +1,99 @@
-//Stores two seperate numbers, one being an interger and the other a string.
-let calculation = [0, "0"];
 //Stores the last operator clicked.
 let operator = "";
 // If calculated is 1 then any number clicked will erase the previous calculated answer.
-let calculated = 0;
+let firstNumber = "";
+let lastNumber = "0";
+let answer = "";
 
 
-//Functionality buttons include Clear, negative/positive and Decimal point
+//Functionality buttons include Clear, negative/positive and Decimal point.
 function buttonFunctions(clicked_id) {
   switch (clicked_id) {
     case "clear":
-      calculation = [0, "0"];
+      firstNumber = "";
+      lastNumber = "0";
       operator = "";
-      document.getElementById("display").innerHTML = calculation[0];
+      answer = "";
+      document.getElementById("display").innerHTML = lastNumber;
       break;
     case "negativePositive":
-      calculation[1] = calculation[1] * -1;
-      document.getElementById("display").innerHTML = calculation[1];
+      lastNumber = lastNumber * -1;
+      document.getElementById("display").innerHTML = lastNumber;
       break;
     case "decimalPoint":
-      calculation[1].includes(".") == true ? "" : calculation[1] = calculation[1] + ".";
-      document.getElementById("display").innerHTML = calculation[1];
+      lastNumber.includes(".") == true ? "" : lastNumber = lastNumber + ".";
+      document.getElementById("display").innerHTML = lastNumber;
   }
 }
 
 //Chosen number is added to the calculation.
 function buttonNumbers(clicked_id) {
-  calculated == 1 ? (calculated = 0, calculation[0] = 0, calculation[1] = "0") : "";
-  calculation[1] == "0" ? calculation[1] = clicked_id : calculation[1] = calculation[1] + clicked_id;
-  calculation[1].length > 9 ? calculation[1] = calculation[1].substring(0,9) : "";
-  document.getElementById("display").innerHTML = calculation[1];
+  answer !== "" || lastNumber == "0" ? (answer = "", lastNumber = "", lastNumber = lastNumber + clicked_id, lastNumber = lastNumber) : lastNumber = lastNumber + clicked_id;
+  lastNumber.length > 9 ? lastNumber = lastNumber.substring(0,9) : "";
+  document.getElementById("display").innerHTML = lastNumber;
 }
 
 // Chosen operator is added to the calculation.
 function buttonOperator(clicked_id) {
-  calculated = 0;
   switch (operator) {
     case "add":
-      calculation[0] == 0 && calculation[1] == "0" ? calculation[0] = 0 : calculation[0] = calculation[0] + parseFloat(calculation[1]);
-      calculation[1] = "0";
-      document.getElementById("display").innerHTML = calculation[0].toString().substring(0,9);
+      firstNumber == 0 && lastNumber == "0" ? firstNumber = 0 : firstNumber = firstNumber + parseFloat(lastNumber);
+      lastNumber = "0";
+      document.getElementById("display").innerHTML = firstNumber.toString().substring(0,9);
       break;
     case "subtract":
-      calculation[0] = calculation[0] - parseFloat(calculation[1]);
-      calculation[1] = "0";
-      document.getElementById("display").innerHTML = calculation[0].toString().substring(0,9);
+      firstNumber == 0 && lastNumber == "0" ? lastNumber = "0" : firstNumber = firstNumber - parseFloat(lastNumber);
+      lastNumber = "0";
+      document.getElementById("display").innerHTML = firstNumber.toString().substring(0,9);
       break;
     case "multiply":
-      calculation[0] == 0 && calculation[1] == "0" ? calculation[0] = 0 : calculation[0] = calculation[0] * parseFloat(calculation[1]);
-      calculation[1] = "0";
-      document.getElementById("display").innerHTML = calculation[0].toString().substring(0,9);
+      firstNumber == 0 && lastNumber == "0" ? firstNumber = 0 : firstNumber = firstNumber * parseFloat(lastNumber);
+      lastNumber = "0";
+      document.getElementById("display").innerHTML = firstNumber.toString().substring(0,9);
       break;
     case "divide":
-      calculation[1] == "0" ? (calculation[0] = 0, document.getElementById("display").innerHTML = "WHAT?!")
-      : (calculation[0] = calculation[0] / parseInt(calculation[1]), document.getElementById("display").innerHTML = calculation[0].toString().substring(0,9));
-      calculation[1] = "0";
+      lastNumber == "0" ? (firstNumber = 0, document.getElementById("display").innerHTML = "ErRoR?!") : 
+      (firstNumber = firstNumber / parseFloat(lastNumber), document.getElementById("display").innerHTML = firstNumber.toString().substring(0,9));
+      lastNumber = "0";
       break;
     case "":
-      calculation[0] = parseFloat(calculation[1]);
-      calculation[1] = "0";
+      answer !== "" ? firstNumber = lastNumber : firstNumber = parseFloat(lastNumber);
+      lastNumber = "0";
   }
   operator = clicked_id;
 }
+
 // Calculates the numbers and operators the user inputted.
 function buttonCalculate() {
-  calculated = 1;
   switch (operator) {
     case "add":
-      document.getElementById("display").innerHTML = (calculation[0] + parseInt(calculation[1])).toString().substring(0,9);
+      answer = firstNumber + parseFloat(lastNumber);
+      document.getElementById("display").innerHTML = answer.toString().substring(0,9);
+      firstNumber = "";
+      lastNumber = answer.toString();
+      operator = "";
       break;
     case "subtract":
-      calculation[0] = calculation[0] - parseInt(calculation[1]);
-      document.getElementById("display").innerHTML = calculation[0].toString().substring(0,9);
-      calculation[1] = 0;
+      answer = firstNumber - parseFloat(lastNumber);
+      document.getElementById("display").innerHTML = answer.toString().substring(0,9);
+      firstNumber = "";
+      lastNumber = answer.toString();
+      operator = "";
       break;
     case "multiply":
-      calculation[0] = calculation[0] * parseInt(calculation[1]);
-      document.getElementById("display").innerHTML = calculation[0].toString().substring(0,9);
-      calculation[1] = 0;
+      answer = firstNumber * parseFloat(lastNumber);
+      document.getElementById("display").innerHTML = answer.toString().substring(0,9);
+      firstNumber = "";
+      lastNumber = answer.toString();
+      operator = "";
       break;
     case "divide":
-      calculation[1] == "0" ? (calculation[0] = 0, document.getElementById("display").innerHTML = "WHAT?!")
-      : (calculation[0] = calculation[0] / parseInt(calculation[1]), document.getElementById("display").innerHTML = calculation[0].toString().substring(0,9));
-      calculation[1] = 0;
+      answer = firstNumber / parseFloat(lastNumber);
+      lastNumber == "0" ? (firstNumber = 0, document.getElementById("display").innerHTML = "ErRoR?!") : 
+      (firstNumber = firstNumber / parseFloat(lastNumber), document.getElementById("display").innerHTML = firstNumber.toString().substring(0,9));
+      firstNumber = "";
+      lastNumber = answer.toString();
+      operator = "";
       break;
     case "":
   }
